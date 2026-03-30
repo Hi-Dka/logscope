69b55849e9797435c911f9f78a942fc41eb028d7
yangxinxin <yangxinxin@bicv.com>
2026-03-30 16:50:55 +0800

    feat(extension): 添加 Prettier 配置和优化激活逻辑
    新增 .prettierrc 文件以配置代码格式化选项。
    优化激活函数中的日志输出和设备变化处理逻辑。

--- src/extension.ts
+++ src/extension.ts
@@ -6,36 +6,40 @@ import { AdbDataSource } from './providers/adb/adb-datasource';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
+    console.log('Congratulations, your extension "hidka" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "hidka" is now active!');
-
-	// Initialize ADB DataSource
+    // Initialize ADB DataSource
     const adbSource = new AdbDataSource();
     context.subscriptions.push(adbSource);
 
     // Subscribe to device changes
     adbSource.onDevicesChanged((devices) => {
-        const deviceList = devices.map(d => `${d.id} (${d.type})`).join(', ');
-        vscode.window.showInformationMessage(`ADB Devices Changed: [${deviceList}]`);
+        const deviceList = devices.map((d) => `${d.id} (${d.type})`).join(', ');
+        vscode.window.showInformationMessage(
+            `ADB Devices Changed: [${deviceList}]`,
+        );
         console.log('Updated Device List:', devices);
     });
 
     // Start tracking
     adbSource.start();
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('hidka.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from logcat!');
-        console.log('Current devices:', adbSource.getDevices());
-	});
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    const disposable = vscode.commands.registerCommand(
+        'hidka.helloWorld',
+        () => {
+            // The code you place here will be executed every time your command is executed
+            // Display a message box to the user
+            vscode.window.showInformationMessage('Hello World from logcat!');
+            console.log('Current devices:', adbSource.getDevices());
+        },
+    );
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
